65086131c60d4396f8ac91723629a9e56c784f5e
Daniel Quah <danielquah148@yahoo.com>
2025-10-22 15:14:04 +1100

    Return a simpler response from drone endpoint

--- src/app/api/v1/drone/route.ts
+++ src/app/api/v1/drone/route.ts
@@ -5,7 +5,15 @@ export async function POST(request: Request) {
     const body = await request.json();
     const { topLeftLat, topLeftLon, bottomRightLat, bottomRightLon } = body;
     const droneHacks = await getDroneInBoundingBox(topLeftLat, topLeftLon, bottomRightLat, bottomRightLon)
-    return new Response(JSON.stringify(droneHacks), {
+    const portals: Portal[] = []
+    droneHacks.map((value) => {
+        portals.push({
+            lat: Number(value.latitude),
+            lon: Number(value.longitude),
+            first_seen_time: value.event_time
+        })
+    })
+    return new Response(JSON.stringify(portals), {
         status: 201,
         headers: { 'Content-Type': 'application/json' }
     });
