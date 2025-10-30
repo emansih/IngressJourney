414c4f97825f984698c8d9185b770e7378d6ae65
Daniel Quah <danielquah148@yahoo.com>
2025-10-31 09:47:13 +1100

    cache recursion data

--- src/app/recursion-history/page.tsx
+++ src/app/recursion-history/page.tsx
@@ -2,13 +2,33 @@
 import { getApexEvents, getLevelingUpTimeline } from "../libs/db"
 import React from "react";
 import { RecursionTimeline } from "../components/timeline/recursion-timeline";
+import { unstable_cache } from "next/cache";
 
 export default async function Page() {
 
-    const levelUpTimeLine = await getLevelingUpTimeline();
-    const apexEvents = await getApexEvents();
+    const CACHE_TIME = 3600
+
+    const levelUpTimelineCache = unstable_cache(
+        async () => {
+            return getLevelingUpTimeline()
+        },
+        ["level_up_timeline"],
+        { tags: ["level_up_timeline_tag"], revalidate: CACHE_TIME }
+    )
+
+    const apexEventsCache = unstable_cache(
+        async () => {
+            return getApexEvents()
+        },
+        ["apex_events"],
+        { tags: ["apex_events_tag"], revalidate: CACHE_TIME }
+    )
+
+
+
+    const [levelUpTimeLine, apexEvents] = await Promise.all([levelUpTimelineCache(), apexEventsCache()])
 
     return (
-        <RecursionTimeline events={levelUpTimeLine} apexEvents={apexEvents}/>
+        <RecursionTimeline events={levelUpTimeLine} apexEvents={apexEvents} />
     )
 }
