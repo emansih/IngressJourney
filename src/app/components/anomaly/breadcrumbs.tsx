e2d1c073dd37b5b41c540c40d69c341964e9329b
Daniel Quah <danielquah148@yahoo.com>
2025-11-09 06:58:47 +1100

    Use single element in breadcrumb component

--- src/app/components/anomaly/breadcrumbs.tsx
+++ src/app/components/anomaly/breadcrumbs.tsx
@@ -9,23 +9,23 @@ interface BreadCrumbsProps {
     }[]
 }
 
-export function BreadCrumbs(breadCrumbProps: BreadCrumbsProps){
-
+export function BreadCrumbs({ breadCrumbs }: BreadCrumbsProps) {
     return (
         <div>
-            <Breadcrumbs aria-label="breadcrumb" style={{ marginLeft: '12px', marginTop: '8px' }}>
-                {breadCrumbProps.breadCrumbs.map(((value) => (
-                    <>
-                        {!value.currentlyActive && (
-                            <Typography sx={{ color: 'text.primary', textDecorationLine: 'underline', cursor: 'pointer' }} onClick={() => {
-                                value.actions()
-                            }}>{value.breadCrumbText}</Typography>
-                        )}
-                        {value.currentlyActive && (
-                            <Typography sx={{ color: 'text.primary' }}>{value.breadCrumbText}</Typography>
-                        )}
-                    </>
-                )))}
+            <Breadcrumbs aria-label="breadcrumb" style={{ marginLeft: 12, marginTop: 8 }}>
+                {breadCrumbs.map((value, index) => (
+                    <Typography
+                        key={index}
+                        sx={{
+                            color: 'text.primary',
+                            textDecorationLine: value.currentlyActive ? 'none' : 'underline',
+                            cursor: value.currentlyActive ? 'default' : 'pointer',
+                        }}
+                        onClick={!value.currentlyActive ? value.actions : undefined}
+                    >
+                        {value.breadCrumbText}
+                    </Typography>
+                ))}
             </Breadcrumbs>
         </div>
     )
